bb55bf89246553ef75d0458c2ee6fc9061242db0
Alancq <netelmer@gmail.com>
2023-03-27 09:22:27 -0400

    Initial commit

---
+++ nucleoF767_tp4/Drivers/API/Inc/API_delay.h
@@ -0,0 +1,29 @@
+/*
+ * API_delay.h
+ *
+ *  Created on: Mar 18, 2023
+ *      Author: alan
+ */
+
+#ifndef API_INC_API_DELAY_H_
+#define API_INC_API_DELAY_H_
+#include <stdint.h>
+#include <stdbool.h>
+#include "stm32f7xx_hal.h"  		/* <- HAL include */
+#include "stm32f7xx_nucleo_144.h" 	/* <- BSP include */
+typedef uint32_t tick_t;
+typedef bool bool_t;
+
+typedef struct{
+   tick_t startTime;
+   tick_t duration;
+   bool_t running;
+} delay_t;
+void delayInit( delay_t * delay, tick_t duration );
+bool_t delayRead( delay_t * delay );
+void delayWrite( delay_t * delay, tick_t duration );
+
+
+//###########################################
+
+#endif /* API_INC_API_DELAY_H_ */
---
+++ practica_3/README.md
@@ -0,0 +1,3 @@
+Práctica 3 
+Autor: E. Alan Cornejo
+Objetivo:Implementar un módulo de software para trabajar con retardos no bloqueantes a partir de las funciones creadas en la práctica 2.
